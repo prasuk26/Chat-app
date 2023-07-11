376da097accdfab8f33a49f49e73ff75248ba63f
Prasuk <Prasukjain2607@gmail.com>
2023-07-12 01:34:56 +0530

    new messages shown but not in a sorted manner

--- src/App.js
+++ src/App.js
@@ -7,57 +7,126 @@ import {
   HStack,
 } from "@chakra-ui/react";
 import Message from "./Components/Message";
-import { getAuth, GoogleAuthProvider, signInWithPopup , onAuthStateChanged,signOut} from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,orderBy
+} from "firebase/firestore";
 import { app } from "./firebaseConfig";
 import { useEffect, useState } from "react";
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 const loginHandler = () => {
   const provider = new GoogleAuthProvider();
   signInWithPopup(auth, provider);
 };
-const logoutHandler = () =>{
+const logoutHandler = () => {
   signOut(auth);
-}
+};
 
 function App() {
-
+  const q = query(collection(db,"Messages"),orderBy("createAt","asc"));
   const [user, setUser] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
-  useEffect( () => {
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      await addDoc(collection(db, "Messages"), {
+        text: message,
+        uid: user.uid,
+        uri: user.photoURL,
+        createdAt: serverTimestamp(),
+      });
+      setMessage("");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
     onAuthStateChanged(auth, (data) => {
       setUser(data);
-    })
-  })
+    });
+  });
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (data) => {
+      setUser(data);
+    });
+
+   const unsubscribeForMessage =  onSnapshot(collection(db, "Messages"), (snap) => {
+  // const unsubscribeForMessage = onSnapshot(q, (snap) => {
+      setMessages(
+        snap.docs.map((item) => {
+          const id = item.id;
+          return { id, ...item.data() };
+        })
+      );
+    });
+    return () => {
+      unsubscribe();
+      unsubscribeForMessage();
+    };
+  });
 
   return (
     <Box bg={"pink"}>
-      { user ? (
-      <Container h={"100vh"} bg={"white"}>
-        <VStack h={"full"} paddingY={"4"}>
-          <Button w={"full"} colorScheme={"red"} onClick={logoutHandler}>
-            Logout
-          </Button>
+      {user ? (
+        <Container h={"100vh"} bg={"white"}>
+          <VStack h={"full"} paddingY={"4"}>
+            <Button w={"full"} colorScheme={"red"} onClick={logoutHandler}>
+              Logout
+            </Button>
 
-          <VStack w={"full"} h={"full"} overflowY={"auto"}>
-            <Message text={"Sample message"} />
-            <Message text={"Sample message"} user={"me"} />
+            <VStack w={"full"} h={"full"} overflowY={"auto"}>
+              {/* <Message text={"Sample message"} />
+              <Message text={"Sample message"} user={"me"} /> */}
+
+              {messages.map((item) => (
+                <Message
+                  key={item.id}
+                  user={item.uri === user.uri ? "me" : "other"}
+                  text={item.text}
+                  uri={item.uri}
+                />
+              ))}
+            </VStack>
+
+            <form style={{ width: "100%" }} onSubmit={submitHandler}>
+              <HStack>
+                <Input
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  placeholder="Enter a message"
+                />
+                <Button colorScheme={"purple"} type="Submit">
+                  Send
+                </Button>
+              </HStack>
+            </form>
           </VStack>
-
-          <form style={{ width: "100%" }}>
-            <HStack>
-              <Input placeholder="Enter a message" />
-              <Button colorScheme={"purple"} type="Submit">
-                Send
-              </Button>
-            </HStack>
-          </form>
+        </Container>
+      ) : (
+        <VStack bg={"white"} justifyContent={"center"} h={"100vh"}>
+          <Button onClick={loginHandler} colorScheme="purple">
+            Sign in With Google
+          </Button>
         </VStack>
-      </Container>
-      ) :
-      <VStack bg={"white"} justifyContent={"center"}  h={"100vh"}>
-        <Button onClick={loginHandler} colorScheme="purple">Sign in With Google</Button>
-      </VStack>}
+      )}
     </Box>
   );
 }
